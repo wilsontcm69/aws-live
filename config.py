f2cb6a2f3020907226f45394fc2d93e0fc86bca4
Wilson Tan <86353208+wilsontcm69@users.noreply.github.com>
2023-08-28 11:49:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c9rfrhg6xyxz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tancherngming-employee"
+customregion = "us-east-1"
+
